335621efef51ab7019f76a9aa603ee467b72300a
Doug Black <dblack@twilio.com>
2016-02-08 21:25:20 -0800

    Initial Commit

---
+++ keylixer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  keylixer
+//
+//  Created by Doug Black on 2/8/16.
+//  Copyright © 2016 Doug Black. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ keylixer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  keylixer
+//
+//  Created by Doug Black on 2/8/16.
+//  Copyright © 2016 Doug Black. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
